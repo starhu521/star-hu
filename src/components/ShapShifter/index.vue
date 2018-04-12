6b2f545dee374b440e15b37ec8ebd82cca85db9c
xingchen.hu <xingchen.hu@ninebot.com>
2018-04-12 18:00:05 +0800

    粒子字体

---
+++ src/components/ShapShifter/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <canvas class="canvas">
+  </canvas>
+</template>
+
+<script>
+import S from './index'
+
+export default {
+  name: 'ShapShifter',
+  props: {
+    text: {
+      type: null,
+      required: false,
+      default: ''
+    }
+  },
+  data() {
+    return {
+      drawing: {
+        canvas: null,
+        context: null,
+        renderFn: null,
+        requestFrame: window.requestAnimationFrame || 
+                      window.webkitRequestAnimationFrame ||
+                      window.mozRequestAnimationFrame ||
+                      window.oRequestAnimationFrame ||
+                      window.msRequestAnimationFrame ||
+                      function(callback) { window.setTimeout(callback, 1000/60) }
+      }
+    }
+  },
+  mounted() {
+    S.init()
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .canvas
+    display inline-block
+</style>
+
+
